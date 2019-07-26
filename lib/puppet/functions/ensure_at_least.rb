c834997db3f3ffb204ae302db79d6c611b52017c
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-07-26 13:00:40 +0200

    lint fixes

--- lib/puppet/functions/ensure_at_least.rb
+++ lib/puppet/functions/ensure_at_least.rb
@@ -3,7 +3,7 @@
 #   The function will compare the currently installed package with the given minimum version and:
 #    - return the minimum version if the installed package is less than the minimum version, or not present
 #    - return the installed version if the installed package is the same or newer than the minimum version
-# 
+#
 # @example Example usage:
 #   # Ensure at least version 2.7.5-77.el7_6 of python is installed
 #     package { 'python':
@@ -17,34 +17,33 @@ Puppet::Functions.create_function(:ensure_at_least, Puppet::Functions::InternalF
   # @param minversion
   #   The minimum version to ensure
   #
-  # @return [String]
-  #   Returns a package version value of type String
-    dispatch :ensure_at_least do
-      scope_param
-      required_param 'String', :package
-      required_param 'String', :minversion
-      return_type 'String'
-    end
-  
-    def ensure_at_least(scope, package, minversion)
-      trusted = closure_scope['trusted']
-      query = "package_inventory[version] {
-                 certname = '#{trusted['certname']}' and
-                 package_name = '#{package}'
-               }"
-  
-      installed = call_function('puppetdb_query', query).map { |elem| elem['version'] }.first
-  
-      if installed
-        case call_function('versioncmp', installed, minversion)
-        when 0, 1
-          installed
-        when -1
-          minversion
-        end
-      else
+  # @return [String] package version value.
+  dispatch :ensure_at_least do
+    scope_param
+    required_param 'String', :package
+    required_param 'String', :minversion
+    return_type 'String'
+  end
+
+  def ensure_at_least(package, minversion)
+    trusted = closure_scope['trusted']
+    query = "package_inventory[version] {
+                certname = '#{trusted['certname']}' and
+                package_name = '#{package}'
+              }"
+
+    installed = call_function('puppetdb_query', query).map { |elem| elem['version'] }.first
+
+    if installed
+      case call_function('versioncmp', installed, minversion)
+      when 0, 1
+        installed
+      when -1
         minversion
       end
+    else
+      minversion
     end
-  
   end
+
+end
