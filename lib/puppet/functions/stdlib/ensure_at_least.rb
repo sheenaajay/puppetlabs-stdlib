fda01923c7f2344942a2191de5235d91e89aefe6
Kevin Reeuwijk <kevin.reeuwijk@puppet.com>
2019-08-05 22:08:35 +0200

    Lint fixes
    Fixed offenses found in https://ci.appveyor.com/project/puppetlabs/puppetlabs-stdlib/builds/26483983/job/rcfogajqds5u9vw7

--- lib/puppet/functions/stdlib/ensure_at_least.rb
+++ lib/puppet/functions/stdlib/ensure_at_least.rb
@@ -25,7 +25,7 @@
 #     }
 #
 # @example To specify all options:
-#   # Ensure the latest version of the openssl gem is installed if it is not at least version 2.1.2 
+#   # Ensure the latest version of the openssl gem is installed if it is not at least version 2.1.2
 #     package { 'openssl':
 #       ensure   => ensure_at_least('openssl', '2.1.2', 'latest', 'gem'),
 #       provider => gem
@@ -55,7 +55,7 @@ Puppet::Functions.create_function(:'stdlib::ensure_at_least') do
 
   def ensure_at_least(package, minversion, installversion = nil, provider = nil)
     # Define default behavior
-    def_providers = { 
+    def_providers = {
       'aix'          => { 'filter' => false, 'type' => 'operatingsystem', 'provider' => 'aix'     },
       'amazon'       => { 'filter' => false, 'type' => 'operatingsystem', 'provider' => 'yum'     },
       'archlinux'    => { 'filter' => false, 'type' => 'operatingsystem', 'provider' => 'pacman'  },
@@ -63,16 +63,16 @@ Puppet::Functions.create_function(:'stdlib::ensure_at_least') do
       'debian'       => { 'filter' => false, 'type' => 'osfamily',        'provider' => 'apt'     },
       'dragonfly'    => { 'filter' => false, 'type' => 'operatingsystem', 'provider' => 'pkgng'   },
       'fedora'       => { 'filter' => true,  'type' => 'operatingsystem', 'fact' => 'operatingsystemmajrelease', 'provider' => [
-        { 'version' => ((19..21).to_a).map(&:to_s), 'provider' => 'yum' },
-        { 'version' => ['default'],   'provider' => 'dnf' }
-      ]},
+        { 'version' => (19..21).to_a.map(&:to_s), 'provider' => 'yum' },
+        { 'version' => ['default'], 'provider' => 'dnf' },
+      ] },
       'freebsd'      => { 'filter' => false, 'type' => 'operatingsystem', 'provider' => 'pkgng'   },
       'gentoo'       => { 'filter' => false, 'type' => 'operatingsystem', 'provider' => 'portage' },
       'redhat'       => { 'filter' => true, 'type' => 'osfamily', 'fact' => 'operatingsystemmajrelease', 'provider' => [
-        { 'version' => ((1..3).to_a).map(&:to_s), 'provider' => 'up2date' },
-        { 'version' => ((4..7).to_a).map(&:to_s), 'provider' => 'yum' },
-        { 'version' => ['default'], 'provider' => 'dnf' }
-      ]},
+        { 'version' => (1..3).to_a.map(&:to_s), 'provider' => 'up2date' },
+        { 'version' => (4..7).to_a.map(&:to_s), 'provider' => 'yum' },
+        { 'version' => ['default'], 'provider' => 'dnf' },
+      ] },
       'hp-ux'        => { 'filter' => false, 'type' => 'operatingsystem', 'provider' => 'hpux'    },
       'mandriva'     => { 'filter' => false, 'type' => 'operatingsystem', 'provider' => 'urpmi'   },
       'mandrake'     => { 'filter' => false, 'type' => 'operatingsystem', 'provider' => 'urpmi'   },
@@ -85,8 +85,8 @@ Puppet::Functions.create_function(:'stdlib::ensure_at_least') do
       'solaris'      => { 'filter' => true, 'type' => 'osfamily', 'fact' => 'kernelrelease', 'provider' => [
         { 'version' => ['5.10'],         'provider' => 'sun' },
         { 'version' => ['5.11', '5.12'], 'provider' => 'pkg' },
-        { 'version' => ['default'],      'provider' => 'pkg' }
-      ]},
+        { 'version' => ['default'],      'provider' => 'pkg' },
+      ] },
       'suse'         => { 'filter' => false, 'type' => 'operatingsystem', 'provider' => 'zypper'  },
       'sles'         => { 'filter' => false, 'type' => 'operatingsystem', 'provider' => 'zypper'  },
       'sled'         => { 'filter' => false, 'type' => 'operatingsystem', 'provider' => 'zypper'  },
@@ -100,9 +100,12 @@ Puppet::Functions.create_function(:'stdlib::ensure_at_least') do
       'package_inventory[version,provider] {',
       "certname = '#{trusted['certname']}'",
       "and package_name = '#{package}'",
-      ("and provider = '#{provider}'" unless provider.nil?),# If a provider was specified, filter on it
+      ("and provider = '#{provider}'" unless provider.nil?), # If a provider was specified, filter on it
       '}',
     ].compact.join(' ')
+
+    # Define core message prefix
+    msgprefix = "stdlib::ensure_at_least | Node: #{trusted['certname']} | Package: #{package} |"
 
     # Query PuppetDB's Package Inventory for matching packages
     queryresult = call_function('puppetdb_query', query)
@@ -113,7 +116,7 @@ Puppet::Functions.create_function(:'stdlib::ensure_at_least') do
       result = { 'version' => '' }
     when 1
       # Exactly one result was found, return this version if it matches the given provider or if no provider was specified
-      if provider.nil? or queryresult.first['provider'] == provider
+      if provider.nil? || queryresult.first['provider'] == provider
         result = queryresult.first
         provider = result['provider']
       else
@@ -122,109 +125,106 @@ Puppet::Functions.create_function(:'stdlib::ensure_at_least') do
     when 2..Float::INFINITY
       # Multiple results were found, which could have multiple causes that we have to test for
       # First, determine if we got multiple records for the same provider, or multiple providers
-      if queryresult.uniq {|e| e['provider']}.length == 1
+      if queryresult.uniq { |e| e['provider'] }.length == 1
         # All the records are for the same provider, so select it if it matches the given provider or if no provider was specified
-        if provider.nil? or queryresult.first['provider'] == provider
-          provider = queryresult.uniq {|e| e['provider']}.first['provider']
+        if provider.nil? || queryresult.first['provider'] == provider
+          provider = queryresult.uniq { |e| e['provider'] }.first['provider']
         else
           result = { 'version' => '' }
         end
-      else
-        # We're dealing with multiple providers now and need to select just one
-        # Only try to determine the default provider if it was not specified in the function call
-        if provider.nil?
-          # If we make it to here, we have this situation:
-          #   - Multiple PuppetDB records were returned by the query
-          #   - These records are coming from >1 providers
-          #   - There could be >1 record for each provider
-          #   - No specific provider was given in the function call, so we have to pick one
-          call_function('notice', "stdlib::ensure_at_least | Node: #{trusted['certname']} | Package: #{package} | Multiple Package Inventory results found and no provider given, attempting to auto-select provider")
-          # First, get all facts so we can query the node's OS
-          facts   = closure_scope['facts']
-          # Iterate through the def_providers array and see if our node matches one based on OS or OS family
-          def_providers.each do |os,data|
-            if (data['type'] == 'operatingsystem' and os == facts['operatingsystem'].downcase) or
-               (data['type'] == 'osfamily' and os == facts['osfamily'].downcase)
-              # Found a def_providers match for this OS/OS Family
-              call_function('notice', "stdlib::ensure_at_least | Node: #{trusted['certname']} | Package: #{package} | Auto-selecting for #{data['type']} #{os}")
-              # Check if this OS (family) uses a different provider dependent on the specific OS version
-              if data['filter']
-                # The provider to select is dependent on the specific OS version
-                found = false
-                def_provider = nil
-                # Iterate through each OS version-specific provider to find a match
-                data['provider'].each do |item|
-                  if item['version'].include? facts[data['fact']]
-                    # Found a match, use this one and exit the loop
-                    found = true
-                    provider = item['provider']
-                    break
-                  elsif item['version'].include? 'default'
-                    # Found a default option, store it for later use
-                    def_provider = item['provider']
-                  end
-                end
-                # Use the default provided we didn't find a version-specific match 
-                if found == false and defined?(def_provider) != nil
-                  provider = def_provider
-                end
-              else
-                # The provider is generic for this OS
-                provider = data['provider']
+      elsif provider.nil?
+        # If we make it to here, we have this situation:
+        #   - Multiple PuppetDB records were returned by the query
+        #   - These records are coming from >1 providers
+        #   - There could be >1 record for each provider
+        #   - No specific provider was given in the function call, so we have to pick one
+        call_function('notice', "#{msgprefix} Multiple Package Inventory results found and no provider given, attempting to auto-select provider")
+        # First, get all facts so we can query the node's OS
+        facts = closure_scope['facts']
+        # Iterate through the def_providers array and see if our node matches one based on OS or OS family
+        def_providers.each do |os, data|
+          blnos = (data['type'] == 'operatingsystem' && os == facts['operatingsystem'].downcase)
+          blnosfamily = (data['type'] == 'osfamily' && os == facts['osfamily'].downcase)
+          next unless blnos || blnosfamily
+          # Found a def_providers match for this OS/OS Family
+          call_function('notice', "#{msgprefix} Auto-selecting for #{data['type']} #{os}")
+          # Check if this OS (family) uses a different provider dependent on the specific OS version
+          if data['filter']
+            # The provider to select is dependent on the specific OS version
+            found = false
+            def_provider = nil
+            # Iterate through each OS version-specific provider to find a match
+            data['provider'].each do |item|
+              if item['version'].include? facts[data['fact']]
+                # Found a match, use this one and exit the loop
+                found = true
+                provider = item['provider']
+                break
+              elsif item['version'].include? 'default'
+                # Found a default option, store it for later use
+                def_provider = item['provider']
               end
-              # No need to parse the rest of the def_providers hash as we already found a match
-              break
             end
+            # Use the default provided we didn't find a version-specific match
+            if found == false && !def_provider.nil?
+              provider = def_provider
+            end
+          else
+            # The provider is generic for this OS
+            provider = data['provider']
           end
+          # No need to parse the rest of the def_providers hash as we already found a match
+          break
         end
       end
-      call_function('notice', "stdlib::ensure_at_least | Node: #{trusted['certname']} | Package: #{package} | Auto-selected provider #{provider}")
+      call_function('notice', "#{msgprefix} Auto-selected provider #{provider}")
+      msgprefix = "#{msgprefix} Provider #{provider} |"
       # Provider is now known or auto-selected, filter the resultset on that provider
       filtered_result = queryresult.select { |elem| elem['provider'] == provider }
       # Now determine how many records we have left
       case filtered_result.length
       when 0
         result = { 'version' => '' }
-        call_function('notice', "stdlib::ensure_at_least | Node: #{trusted['certname']} | Package: #{package} | Provider #{provider} | No packages found")
+        call_function('notice', "#{msgprefix} No packages found")
       when 1
         result = filtered_result.first
-        call_function('notice',  "stdlib::ensure_at_least | Node: #{trusted['certname']} | Package: #{package} | Provider #{provider} | Found version #{result['version']}")
+        call_function('notice', "#{msgprefix} Found version #{result['version']}")
       when 2..Float::INFINITY
         # We found multiple versions for this package, need to recursively compare them to find the highest version
-        call_function('notice', "stdlib::ensure_at_least | Node: #{trusted['certname']} | Package: #{package} | Provider #{provider} | Found multiple versions, auto-selecting highest")
+        call_function('notice', "#{msgprefix} Found multiple versions, auto-selecting highest")
         highest = ''
         filtered_result.each do |item|
           case call_function('versioncmp', item['version'], highest)
-          when 0,1
+          when 0, 1
             highest = item['version']
           else
             highest = highest
           end
         end
         result = { 'version' => highest }
-        call_function('notice', "stdlib::ensure_at_least | Node: #{trusted['certname']} | Package: #{package} | Provider #{provider} | Auto-selected highest version #{highest}")
+        call_function('notice', "#{msgprefix} Auto-selected highest version #{highest}")
       else
         result = { 'version' => '' }
-        call_function('notice', "stdlib::ensure_at_least | Node: #{trusted['certname']} | Package: #{package} | Provider #{provider} | Error occured finding packages")
+        call_function('notice', "#{msgprefix} Error occured finding packages")
       end
     else
       result = { 'version' => '' }
-      call_function('notice', "stdlib::ensure_at_least | Node: #{trusted['certname']} | Package: #{package} | Provider #{provider} | Error occured finding packages")
+      call_function('notice', "#{msgprefix}#{" Provider #{provider} |" unless provider.nil?} Error occured finding packages")
     end
 
     installed = result['version']
 
     case call_function('versioncmp', installed, minversion)
     when 0, 1
-      call_function('notice', "stdlib::ensure_at_least | Node: #{trusted['certname']} | Package: #{package} | Provider #{provider} | Returned installed version #{installed}, as it was newer or same as minimum version #{minversion}")
+      call_function('notice', "#{msgprefix}#{" Provider #{provider} |" unless provider.nil?} Returned installed version #{installed}, reason: newer or same as minimum version #{minversion}")
       return installed
     else
-      if installversion.nil?
-        retversion = minversion
+      retversion = if installversion.nil?
+        minversion
       else
-        retversion = installversion
+        installversion
       end
-      call_function('notice', "stdlib::ensure_at_least | Node: #{trusted['certname']} | Package: #{package} |#{unless provider.nil? then " Provider #{provider} |" end} Returned version to install: #{retversion}, as minimum version of #{minversion} was not installed")
+      call_function('notice', "#{msgprefix}#{" Provider #{provider} |" unless provider.nil?} Returned version to install: #{retversion}, reason: minimum version #{minversion} not present")
       return retversion
     end
   end
